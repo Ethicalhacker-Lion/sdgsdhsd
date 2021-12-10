5ba11c2871108d8ce1253f8a7f3237e5a2e9621f
Ethicalhacker-Lion <74255405+Ethicalhacker-Lion@users.noreply.github.com>
2021-12-10 16:37:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDP1Ips8Pz0ZuX3OAIBJX4DnWY-y013Ovc",
+    authDomain: "hhhfhhfhfhfhf.firebaseapp.com",
+    databaseURL: "https://hhhfhhfhfhfhf-default-rtdb.firebaseio.com",
+    projectId: "hhhfhhfhfhfhf",
+    storageBucket: "hhhfhhfhfhfhf.appspot.com",
+    messagingSenderId: "946973987755",
+    appId: "1:946973987755:web:c24fcff203193bcef8b8ee"
+  };
